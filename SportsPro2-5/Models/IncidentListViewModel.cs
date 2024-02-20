1a023cdac9fe6fb4e6646e4e6b2236c4c71fdbc3
gnarlsley <thompson.garrett7413@gmail.com>
2024-02-19 19:58:38 -0600

    Addition of view models.

---
+++ SportsPro2-5/Models/IncidentListViewModel.cs
@@ -0,0 +1,22 @@
+﻿using SportsPro2_5.ViewModels;
+using SportsPro.Models;
+
+namespace SportsPro2_5.Models
+{
+    public class IncidentListViewModel : IncidentViewModel
+    {
+        public List<Incident> Incidents { get; set; }
+        public List<Product> Products { get; set; }
+        public List<Technician> Technicians { get; set; }
+        public List<Customer> Customers { get; set; }
+
+        public string action = string.Empty;
+
+
+        //this code is not working
+
+        /*        public string checkAction(string c) =>
+                    c.ToLower() == Viewbag.Action.ToLower() ? "Add" : "Edit";
+            */
+    }
+}
---
+++ SportsPro2-5/Models/IncidentViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.CodeAnalysis.Editing;
+using SportsPro.Models;
+using System.Collections.Generic;
+namespace SportsPro2_5.ViewModels;
+
+public class IncidentViewModel {
+
+    public List<Incident> Incidents { get; set; }
+    public string IncidentState { get; set; } = "all";
+}
