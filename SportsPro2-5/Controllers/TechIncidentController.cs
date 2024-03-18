90242cd1b72ffd8c03c40e9f16663172b1129463
Ethan <ethanjackniehus@gmail.com>
2024-03-18 10:09:30 -0500

    Build edit button and started on the list.cshtml

--- SportsPro2-5/Controllers/TechIncidentController.cs
+++ SportsPro2-5/Controllers/TechIncidentController.cs
@@ -1,21 +1,53 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsPro.Models;
+using System.Linq;
 
 namespace SportsPro2_5.Controllers
 {
     public class TechIncidentController : Controller
     {
-
         private readonly SportsProContext _context;
 
         public TechIncidentController(SportsProContext context)
         {
             _context = context;
         }
+
+        
         public IActionResult Index()
         {
             ViewBag.Technicians = _context.Technicians.OrderBy(t => t.Name).ToList();
             return View();
         }
+
+        // Display form for editing an incident
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var incident = _context.Incidents.Find(id);
+
+            if (incident == null)
+            {
+                return NotFound();
+            }
+
+            return View(incident);
+        }
+
+        // Handle submission of edited incident form
+        [HttpPost]
+        public IActionResult Edit(Incident incident)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(incident);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(incident);
+        }
+
+       
     }
 }
